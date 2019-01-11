75d22d4e38815f12a4fe4c8f60fb326b25e7f78e
Peter Mei <peter.mei@autoalert.com>
2019-01-11 15:04:57 -0800

    update

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('Hello world');
+var x = 1;
+var y = 0.2;
+
+console.log(x + y);
+
+var msgs = require("./msgs.js");
+
+console.log(msgs.first);
---
+++ NodejsConsoleApp/msgs.js
@@ -0,0 +1,1 @@
+﻿exports.first = "This is the first message";
